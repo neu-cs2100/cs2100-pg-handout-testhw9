5d8ca1f1c72c756b6d7ff95bcaf41c4f7d02033b
Smaran-Teja-P <smaranteja32@gmail.com>
2025-08-19 11:19:54 -0400

    disable assignment from no return pylint error, add coveragerc

--- generate_coverage_reports.sh
+++ generate_coverage_reports.sh
@@ -4,7 +4,7 @@
 COVERAGE_DIR="coverage_reports"
 
 # Run tests with coverage
-coverage run --omit=".venv/*" test_runner.py
+coverage run test_runner.py
 
 # Generate HTML report
 coverage html -d "$COVERAGE_DIR"
